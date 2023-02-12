87e53f454c056e3d67e48353c8ae27c45a2fb19b
fmigg <fmigg@Fernando>
2023-02-12 23:31:31 +0100

    Casi terminado

--- EjerciciosPracticos-Tema5/src/buscaminas/Main.java
+++ EjerciciosPracticos-Tema5/src/buscaminas/Main.java
@@ -9,62 +9,68 @@ public class Main {
 		Scanner read = new Scanner(System.in);
 
 		String tabla[] = new String[20];
-		String incognita[] = new String[20];
+		String incognita[] = { "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x",
+				"x", "x" };
 		int posicionMina;
-		int casillaUsuario;
+		int casillaUsuario = 0;
 
-		tabla[0]="0";
-		tabla[19]="0";
+		tabla[0] = "0";
+		tabla[19] = "0";
 		for (int contador = 1; contador <= 6; contador++) {
 			posicionMina = (int) ((Math.random() * 19) + 1);
-			while (tabla[posicionMina]=="*") {
+			while (tabla[posicionMina] == "*") {
 				posicionMina = (int) ((Math.random() * 19) + 1);
 			}
-			
-			tabla[posicionMina-1] = "*";
 
-			
+			tabla[posicionMina - 1] = "*";
+
 			System.out.println(posicionMina);
 		}
 
 		for (int i = 0; i < tabla.length; i++) {
 
-			if (i == 0 && tabla[i]!="*") {
+			if (i == 0 && tabla[i] != "*") {
 				if (tabla[i + 1] == "*") {
 					tabla[i + 1] = "1";
 				}
 			}
 
-			else if (i == 19 && tabla[i]!="*") {
+			else if (i == 19 && tabla[i] != "*") {
 				if (tabla[i - 1] == "*") {
 					tabla[i] = "1";
 				}
 			}
 
-			else if ((i!=0&&i!=19)&&(tabla[i - 1] == "*" && tabla[i + 1] == "*") && tabla[i]!="*") {
+			else if ((i != 0 && i != 19) && (tabla[i - 1] == "*" && tabla[i + 1] == "*") && tabla[i] != "*") {
 				tabla[i] = "2";
 			}
 
-			else if ((i!=0&&i!=19)&&(tabla[i - 1] == "*" || tabla[i + 1] == "*") && tabla[i]!="*") {
+			else if ((i != 0 && i != 19) && (tabla[i - 1] == "*" || tabla[i + 1] == "*") && tabla[i] != "*") {
 				tabla[i] = "1";
 			}
 
-			else if (tabla[i]!="*") {
+			else if (tabla[i] != "*") {
 				tabla[i] = "0";
 			}
 
 		}
 
 		System.out.println(Arrays.toString(tabla));
-	
-		for (int i = 0; i<incognita.length;i++) {
-			
-		}
 
-		System.out.println("Que casilla desea destapar [1-20]: ");
-		casillaUsuario = read.nextInt();
-		
-		
+		System.out.println(Arrays.toString(incognita));
 
+		int contadorX = 20;
+			do {
+				System.out.println("Que casilla desea destapar [1-20]: ");
+				casillaUsuario = read.nextInt();
+				incognita[casillaUsuario - 1] = tabla[casillaUsuario - 1];
+				contadorX--;
+				System.out.println(Arrays.toString(incognita));
+				if (contadorX == 6 && incognita[casillaUsuario - 1] != "*") {
+					System.out.println("¡Has ganado!");
+					break;
+				}
+			} while (incognita[casillaUsuario - 1] != "*");
+			System.out.println("Has perdido...");
 	}
 }
