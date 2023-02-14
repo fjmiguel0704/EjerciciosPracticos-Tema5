cdb47a123e07b7966caff0ad271f0f378ab61d34
fjmiguel0704 <fernandoj.miguel@iesnervion.es>
2023-02-14 14:08:23 +0100

    Terminado en el main

--- EjerciciosPracticos-Tema5/src/sieteymedia/Main.java
+++ EjerciciosPracticos-Tema5/src/sieteymedia/Main.java
@@ -9,22 +9,25 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner read = new Scanner(System.in);
 
-		double valor = 0;
+		double valorJ1 = 0;
+		double valorJ2 = 0;
 		int palo;
 		int numero;
 		String paloEscrito = "";
-		String respuesta = "";
+		String respuestaJ1 = "";
+		String respuestaJ2 = "";
 
 		System.out.println("Turno de jugador 1");
 		System.out.println("¿Quieres sacar una carta? s/n");
-		respuesta = read.next();
-		while (respuesta.equals("s")) {
+		respuestaJ1 = read.next();
+
+		while (respuestaJ1.equals("s")) {
 			numero = (int) (Math.random() * 10) + 1;
 
 			if (numero == 8 || numero == 9 || numero == 10) {
-				valor += 0.5;
+				valorJ1 += 0.5;
 			} else {
-				valor += numero;
+				valorJ1 += numero;
 			}
 
 			palo = (int) (Math.random() * 4) + 1;
@@ -45,13 +48,70 @@ public class Main {
 			}
 
 			System.out.println("Tu carta es: " + numero + " de " + paloEscrito);
-			System.out.println("Tienes " + valor + " puntos");
-			if (valor > 7.5) {
+			System.out.println("Tienes " + valorJ1 + " puntos");
+			if (valorJ1 > 7.5) {
 				System.out.println("Has perdido, te has pasado...");
+				System.out.println("Ha ganado Jugador 2");
 				break;
 			}
 			System.out.println("¿Quieres seguir jugando? s/n");
-			respuesta = read.next();
+			respuestaJ1 = read.next();
+		}
+
+		if (respuestaJ1.equals("n")) {
+			System.out.println("Turno de jugador 2");
+			System.out.println("¿Quieres sacar una carta? s/n");
+			respuestaJ2 = read.next();
+			while (respuestaJ2.equals("s")) {
+				numero = (int) (Math.random() * 10) + 1;
+
+				if (numero == 8 || numero == 9 || numero == 10) {
+					valorJ2 += 0.5;
+				} else {
+					valorJ2 += numero;
+				}
+
+				palo = (int) (Math.random() * 4) + 1;
+				if (palo == 1) {
+					paloEscrito = "copa";
+				}
+
+				if (palo == 2) {
+					paloEscrito = "oro";
+				}
+
+				if (palo == 3) {
+					paloEscrito = "espada";
+				}
+
+				if (palo == 4) {
+					paloEscrito = "basto";
+				}
+
+				System.out.println("Tu carta es: " + numero + " de " + paloEscrito);
+				System.out.println("Tienes " + valorJ2 + " puntos");
+				if (valorJ2 > 7.5) {
+					System.out.println("Has perdido, te has pasado...");
+					System.out.println("Ha ganado Jugador 1");
+					break;
+				}
+				System.out.println("¿Quieres seguir jugando? s/n");
+				respuestaJ2 = read.next();
+			}
+		}
+
+		if (respuestaJ2.equals("n")) {
+			if (valorJ1 > valorJ2) {
+				System.out.println("Ha ganado Jugador 1");
+			}
+
+			else if (valorJ2 > valorJ1) {
+				System.out.println("Ha ganado Jugador 2");
+			}
+
+			if (valorJ1 == valorJ2) {
+				System.out.println("Empate");
+			}
 		}
 	}
 
